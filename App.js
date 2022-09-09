9f23a81bc433261043f5f620aa4a10f0f3f7a87e
zainabahmed8907 <zaahmedmughal@gmail.com>
2022-09-09 15:19:52 +0500

    refresh-control and list

--- App.js
+++ App.js
@@ -1,18 +1,51 @@
 import { useState } from "react";
-import { Button, Linking, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  Linking,
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [items, setItems] = useState([
+    { key: 1, text: "1" },
+    { key: 2, text: "2" },
+    { key: 3, text: "3" },
+    { key: 4, text: "4" },
+    { key: 5, text: "4" },
+    { key: 6, text: "6" },
+    { key: 7, text: "7" },
+    { key: 8, text: "8" },
+    { key: 9, text: "9" },
+    { key: 10, text: "10" },
+    { key: 11, text: "11" },
+  ]);
+  const [refresh, seRefresh] = useState(false);
 
-
+  const pageRefresh = () => {
+    seRefresh(true);
+    setItems([...items, { key: 12, text: "added Item 12" }]);
+    seRefresh(false);
+  };
   return (
     <View style={styles.container}>
-      <View style={styles.view1}>
-        <Text>1</Text>
-      </View>
-      <View style={styles.view2}>
-        <Text>1</Text>
-      </View>
-
+      <ScrollView
+        refreshControl={
+          <RefreshControl onRefresh={pageRefresh} refreshing={refresh} />
+        }
+      >
+        {items.map((item) => {
+          return (
+            <View key={item.key} style={styles.items}>
+              <Text style={item.text}>{item.text}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -21,36 +54,16 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
     flex: 1,
-    justifyContent: "space-between",
     flexDirection: "column",
-    margin: 30,
-
-
   },
-  view1: {
-    width: "100%",
-    padding: 10,
-    backgroundColor: "yellow",
-    flex: 1,
-    height: '80%',
-    borderColor: "red",
-    borderWidth: 10,
-    justifyContent: "center",
-    alignItems: "center",
-
+  items: {
+    backgroundColor: "purple",
+    margin: 10,
+    padding: 20,
   },
-  view2: {
-    width: "100%",
-    padding: 10,
-    backgroundColor: "green",
-    flex: 2,
-    height: '80%',
-    borderColor: "blue",
-    borderWidth: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    marginVertical: 10
-
-  }
-
+  text: {
+    color: "white",
+    fontSize: 20,
+    fontStyle: "italic",
+  },
 });
