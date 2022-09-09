85e92fccc8e466c4fa8101f441ab3ba81b87c07e
zainabahmed8907 <zaahmedmughal@gmail.com>
2022-09-09 14:58:03 +0500

    rn flexbox example

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, Linking, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.view1}>
+        <Text>1</Text>
+      </View>
+      <View style={styles.view2}>
+        <Text>1</Text>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#fff",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "space-between",
+    flexDirection: "column",
+    margin: 30,
+
+
   },
+  view1: {
+    width: "100%",
+    padding: 10,
+    backgroundColor: "yellow",
+    flex: 1,
+    height: '80%',
+    borderColor: "red",
+    borderWidth: 10,
+    justifyContent: "center",
+    alignItems: "center",
+
+  },
+  view2: {
+    width: "100%",
+    padding: 10,
+    backgroundColor: "green",
+    flex: 2,
+    height: '80%',
+    borderColor: "blue",
+    borderWidth: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 10
+
+  }
+
 });
